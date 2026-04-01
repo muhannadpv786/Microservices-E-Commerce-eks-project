a018e00659c12151b7390d779a63e84b13087f58
MUHANNED PV <p.vmuhannad786@gmail.com>
2026-04-01 13:23:54 +0530

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "muhannad1-eks"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "muhannad1-eks"
     Environment = "dev"
   }
 }
@@ -19,10 +19,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "muhannad1-eks"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "muhannad1-eks"
     Environment = "dev"
   }
 }
